bd4d61856d6ca5668fde9a36983667ac3f31bfec
Viktoryia Zhybul <victoria.zhibul@gmail.com>
2023-04-17 23:10:30 +0300

    del: comments

--- src/dream-team.js
+++ src/dream-team.js
@@ -11,29 +11,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
- *
- */
-// function createDreamTeam(members) {
-//   let teamName = "";
-//   let name = "";
-//   for (let i = 0; i < members.length; i++) {
-//     if ((typeof(members[i]) === "string")) {
-//       let oneMemberName = members[i].split(" ");
-//       console.debug(oneMemberName);
-//       if (oneMemberName.length > 1) {
-//         for (let j = 0; j < oneMemberName.length; j++) {
-//           teamName += oneMemberName[j].substring(0).toUpperCase();
-//         }
-//       } else {
-//         teamName += oneMemberName[0].substring(0).toUpperCase();
-//       }
-//      name = teamName.split("").sort().join("");
-//      return name;
-//     } else {
-//       return false;
-//     }
-//   }
-// }
+*/
 function createDreamTeam( members ) {
   let letters = "";
   let result = "";
