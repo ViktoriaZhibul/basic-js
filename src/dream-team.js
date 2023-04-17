841d696c950f0f9605be7ca4013c43c77379c09f
Viktoryia Zhybul <victoria.zhibul@gmail.com>
2023-04-17 23:08:57 +0300

    add: dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// function createDreamTeam(members) {
+//   let teamName = "";
+//   let name = "";
+//   for (let i = 0; i < members.length; i++) {
+//     if ((typeof(members[i]) === "string")) {
+//       let oneMemberName = members[i].split(" ");
+//       console.debug(oneMemberName);
+//       if (oneMemberName.length > 1) {
+//         for (let j = 0; j < oneMemberName.length; j++) {
+//           teamName += oneMemberName[j].substring(0).toUpperCase();
+//         }
+//       } else {
+//         teamName += oneMemberName[0].substring(0).toUpperCase();
+//       }
+//      name = teamName.split("").sort().join("");
+//      return name;
+//     } else {
+//       return false;
+//     }
+//   }
+// }
+function createDreamTeam( members ) {
+  let letters = "";
+  let result = "";
+  if (!Array.isArray(members)){
+    return false;
+  } else {
+    for (let i = 0; i < members.length; i++) {
+      if(typeof(members[i]) === "string"){
+        letters += members[i].trim()[0].toUpperCase();
+      }
+    }
+    result = letters.split("").sort().join("");
+    return result;
+  }
 }
+
+createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']); //=> 'ADMM'
+createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]); //=> 'LOO'
 
 module.exports = {
   createDreamTeam
